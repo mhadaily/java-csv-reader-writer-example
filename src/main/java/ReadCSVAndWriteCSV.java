b280f3c6179faca54cd0ad87b48a00c2acd7fe04
Majid <mhadaily@gmail.com>
2018-04-09 18:12:28 +0200

    initial commit

--- src/main/java/ReadCSVAndWriteCSV.java
+++ src/main/java/ReadCSVAndWriteCSV.java
@@ -8,9 +8,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
+/**
+ * THIS IS NOT THE BEST WHEN CSV IS HUGE, AS I AM KEEPING DATA IN THE MEMORY AND IT:S NOT A GOOD APPROACH,
+ * HOWEVER, IT'S FASTEST WAY FOR SMALLER CSV
+ */
 public class ReadCSVAndWriteCSV {
     private static final String CSV_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/tweets.csv";
     private static final String CSV_RESULT_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/tweets_tag.csv";
+    private static final String WORRY_TAG = "WORRY";
+    private static final String NOWORRY_TAG = "NOWORRY";
 
     public static void main(String[] args) throws IOException {
         List<String[]> myEntries = readAllRecordsAtOnce();
@@ -20,20 +26,6 @@ public class ReadCSVAndWriteCSV {
             testedEntries.add(withTestResult);
         }
         writeToFile(testedEntries);
-    }
-
-    private static void readRecordsOneByOne() throws IOException { // WE DONT NEED THIS FUNCTION
-        try (
-                Reader reader = Files.newBufferedReader(Paths.get(CSV_FILE_PATH));
-                CSVReader csvReader = new CSVReader(reader);
-        ) {
-            // Reading Records One by One in a String array
-            String[] nextRecord;
-            while ((nextRecord = csvReader.readNext()) != null) {
-                System.out.println("Tweet : " + nextRecord[0]);
-                System.out.println("==========================");
-            }
-        }
     }
 
     private static List<String[]> readAllRecordsAtOnce() throws IOException {
@@ -53,7 +45,7 @@ public class ReadCSVAndWriteCSV {
         String tweet = row[0];
         List<String> words = Arrays.asList("HIV", "flu");// YOU CAN ADD YOUR WORDS HERE
         Boolean match = words.stream().anyMatch(tweet::contains);
-        return match ? "WORRY" : "NOWORRY";
+        return match ? WORRY_TAG : NOWORRY_TAG;
     }
 
     private static void writeToFile(List<String[]> myEntries) throws IOException {
